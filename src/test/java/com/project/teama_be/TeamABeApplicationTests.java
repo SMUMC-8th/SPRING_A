1fe8d4712d000a5db8c71cdf9131e3dd57206796
Jimyeong Kim <kjm021221@icloud.com>
2025-03-28 00:49:45 +0900

    🎉Begin a Project

---
+++ src/test/java/com/project/teama_be/TeamABeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.teama_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamABeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
