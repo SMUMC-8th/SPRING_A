c08288b881c598b9f559355fa586c95119916a92
Jimyeong Kim <kjm021221@icloud.com>
2025-05-01 18:13:50 +0900

    ✨feat: 응답정규화 및 Swagger 설정

---
+++ src/main/java/com/project/teama_be/global/apiPayload/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.project.teama_be.global.apiPayload.exception;
+
+import com.project.teama_be.global.apiPayload.code.BaseErrorCode;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException{
+
+    private final BaseErrorCode code;
+
+    public CustomException(BaseErrorCode errorCode) {
+        this.code = errorCode;
+    }
+}
