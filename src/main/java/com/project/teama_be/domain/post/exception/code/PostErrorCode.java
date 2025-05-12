9247f84f109e9cf9ae1115d6ccc7b8d9aaeea25a
Juheon <juheon0313@naver.com>
2025-05-13 05:20:06 +0900

    ✨ feat: 키워드 검색 예외 처리, 응답 통일

--- src/main/java/com/project/teama_be/domain/post/exception/code/PostErrorCode.java
+++ src/main/java/com/project/teama_be/domain/post/exception/code/PostErrorCode.java
@@ -11,8 +11,13 @@ public enum PostErrorCode implements BaseErrorCode {
 
     NOT_FOUND(HttpStatus.NOT_FOUND,
             "POST404_0",
-            "해당 게시글이 존재하지 않습니다.")
-    ;
+            "해당 게시글이 존재하지 않습니다."),
+    NOT_FOUND_KEYWORD(HttpStatus.NOT_FOUND,
+                      "POST404_1",
+            "해당 키워드의 게시글이 존재하지 않습니다."),
+    NOT_VALID_TYPE(HttpStatus.BAD_REQUEST,
+            "VALID400_1",
+            "잘못된 키워드 종류입니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
