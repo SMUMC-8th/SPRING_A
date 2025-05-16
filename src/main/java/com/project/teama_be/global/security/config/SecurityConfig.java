cdf87d781154c0af1ce1a64bcede307b935c08d2
Jimyeong Kim <kjm021221@icloud.com>
2025-05-16 19:17:41 +0900

    ♻️refactor: SecurityConfig 에 CustomLogoutHandler 응답 통일

--- src/main/java/com/project/teama_be/global/security/config/SecurityConfig.java
+++ src/main/java/com/project/teama_be/global/security/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package com.project.teama_be.global.security.config;
 
 import com.project.teama_be.domain.member.repository.MemberRepository;
+import com.project.teama_be.domain.member.service.JwtTokenService;
 import com.project.teama_be.global.config.CorsConfig;
+import com.project.teama_be.global.security.exception.JwtAccessDeniedHandler;
+import com.project.teama_be.global.security.exception.JwtAuthenticationEntryPoint;
 import com.project.teama_be.global.security.filter.CustomLoginFilter;
 import com.project.teama_be.global.security.filter.JwtAuthorizationFilter;
+import com.project.teama_be.global.security.handler.CustomLogoutHandler;
 import com.project.teama_be.global.security.util.JwtUtil;
+import com.project.teama_be.global.utils.HttpResponseUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -18,7 +24,8 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -28,6 +35,9 @@ public class SecurityConfig {
     private final AuthenticationConfiguration authenticationConfiguration;
     private final JwtUtil jwtUtil;
     private final MemberRepository memberRepository;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final JwtTokenService jwtTokenService;
 
     //인증이 필요하지 않은 url
     private final String[] allowedUrls = {
@@ -36,6 +46,9 @@ public class SecurityConfig {
             "/v3/api-docs/**",
             "/api/auth/signup",
             "/api/auth/login",
+            "/api/auth/refresh",
+            "/api/members/check-id",
+            "/api/members/check-nickname",
             "/ws-stomp/**",  // WebSocket 관련 모든 경로 추가
             "/ws-stomp/info", // SockJS의 정보 엔드포인트 추가
             "/oauth2/callback/kakao",
@@ -61,11 +74,12 @@ public class SecurityConfig {
                         .configurationSource(CorsConfig.apiConfigurationSource()));
 
         // CSRF 토큰 설정 - 쿠키 사용 시 필요
-        http
-                .csrf(csrf -> csrf
-                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                        .ignoringRequestMatchers("/api/auth/login", "/api/auth/signup")
-                );
+//        http
+//                .csrf(csrf -> csrf
+//                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+//                        .ignoringRequestMatchers("/api/auth/login", "/api/auth/signup")
+//                );
+        http.csrf(AbstractHttpConfigurer::disable); // 테스트 용도로만!
 
         // form 로그인 방식 비활성화 -> REST API 로그인을 사용할 것이기 때문에
         http
@@ -79,6 +93,12 @@ public class SecurityConfig {
         http
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        http
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                        .accessDeniedHandler(jwtAccessDeniedHandler)
+                );
 
         // OAuth2 로그인 설정 추가
         http
@@ -104,6 +124,25 @@ public class SecurityConfig {
         // JwtFilter를 CustomLoginFilter 앞에서 동작하도록 필터 체인에 추가
         http
                 .addFilterBefore(new JwtAuthorizationFilter(jwtUtil, memberRepository), CustomLoginFilter.class);
+
+        // Logout Handler 추가
+        http
+                .logout(logout -> logout
+                        .logoutUrl("/api/auth/logout")
+                        .addLogoutHandler(new CustomLogoutHandler(jwtTokenService, jwtUtil))
+                        .logoutSuccessHandler((request, response, authentication) -> {
+                            try {
+                                HttpResponseUtil.setSuccessResponse(
+                                        response,
+                                        HttpStatus.OK,
+                                        "로그아웃이 완료되었습니다."
+                                );
+                            } catch (IOException e) {
+                                // 예외 처리
+                                response.setStatus(HttpStatus.INTERNAL_SERVER_ERROR.value());
+                            }
+                        })
+                );
 
 
         return http.build();
