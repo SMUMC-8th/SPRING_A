4147db6caf7e90ef6002a96cda6d9b8325b9d9f7
nomad1jin <yj007shin@gmail.com>
2025-06-16 22:27:48 +0900

    ✨ feat: 메인화면에서 사용자 위치 중심 게시글 리턴

--- src/main/java/com/project/teama_be/domain/location/repository/LocationRepository.java
+++ src/main/java/com/project/teama_be/domain/location/repository/LocationRepository.java
@@ -3,8 +3,18 @@ package com.project.teama_be.domain.location.repository;
 import com.project.teama_be.domain.location.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface LocationRepository extends JpaRepository<Location, Long> {
     boolean existsByPlaceName(String s);
 
     Location findByPlaceName(String s);
+
+    List<Location> findByLatitudeBetweenAndLongitudeBetween(
+            BigDecimal minLat,
+            BigDecimal maxLat,
+            BigDecimal minLng,
+            BigDecimal maxLng
+    );
 }
--- src/main/java/com/project/teama_be/domain/post/repository/PostRepository.java
+++ src/main/java/com/project/teama_be/domain/post/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.project.teama_be.domain.post.repository;
 
+import com.project.teama_be.domain.location.entity.Location;
 import com.project.teama_be.domain.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import java.util.Optional;
 public interface PostRepository extends JpaRepository<Post, Long>, PostQueryDsl {
 
     Optional<Post> findPostById(Long postId);
+
+    Post findTopByLocationOrderByCreatedAtDesc(Location location);
 }
